10edf24945068296ece8871a8511c83cb8a8c6f4
Winter Olali <olaliwinter@gmail.com>
2026-04-11 11:55:26 +0300

    Update Dashboard.jsx

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,27 @@
-function Dashboard() {
+import { useEffect, useState } from "react";
+import axios from "../api/axios";
+
+export default function Dashboard() {
+  const [membersCount, setMembersCount] = useState(0);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    const res = await axios.get("/members");
+    setMembersCount(res.data.length);
+  };
+
   return (
     <div>
       <h1>Chama Dashboard</h1>
+
+      <div style={{ display: "flex", gap: "20px" }}>
+        <div>Members: {membersCount}</div>
+        <div>Contributions: coming soon</div>
+        <div>Loans: coming soon</div>
+      </div>
     </div>
   );
 }
-
-export default Dashboard;
