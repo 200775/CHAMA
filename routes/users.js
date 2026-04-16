d564aaade2979c3ee2dc06c6ccd722fc917adf80
Winter Olali <olaliwinter@gmail.com>
2026-04-16 21:50:48 +0300

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../config/db");
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email, password });
+
+  if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+  res.json({
+    message: "Login successful",
+    user
+  });
+});
+
+module.exports = router;
