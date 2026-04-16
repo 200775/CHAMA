bc5c17583a3f596fc7eee05f1c45aa26e3cf1ba0
Winter Olali <olaliwinter@gmail.com>
2026-04-16 21:49:54 +0300

    Add files via upload

---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const res = await axios.post("https://chamazyl.up.railway.app/", {
+      email,
+      password
+    });
+
+    alert(res.data.message);
+    localStorage.setItem("user", JSON.stringify(res.data.user));
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
