b99e7799d14b153e19a373a25d4567dd697fb763
Winter Olali <olaliwinter@gmail.com>
2026-04-16 21:35:13 +0300

    Update Members.jsx

--- Frontend/src/pages/Members.jsx
+++ Frontend/src/pages/Members.jsx
@@ -19,6 +19,7 @@ export default function Members() {
   const addMember = async () => {
     await axios.post("/members", { name });
     setName("");
+    alert("Member added!");
     fetchMembers();
   };
 
