3e47d01c391db466612d78a4c5a239ba710ca720
Winter Olali <olaliwinter@gmail.com>
2026-04-18 19:42:08 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../config/db");
+const User = require("../models/User");
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
