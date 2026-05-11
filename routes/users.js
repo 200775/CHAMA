d35154c20caf5948b9732d51e0066c903f9d0d3b
Winter Olali <olaliwinter@gmail.com>
2026-05-11 14:02:16 +0300

    updated project

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const router = express.Router();
 const User = require("../config/mongo");
@@ -16,23 +15,3 @@ router.post("/login", async (req, res) => {
 });
 
 module.exports = router;
-=======
-const express = require("express");
-const router = express.Router();
-const User = require("../config/mongo");
-
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email, password });
-
-  if (!user) return res.status(400).json({ message: "Invalid credentials" });
-
-  res.json({
-    message: "Login successful",
-    user
-  });
-});
-
-module.exports = router;
->>>>>>> 93c28912e91cce27073d9b5b411a6f452c8722b0
