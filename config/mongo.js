d6a2371ccfadfe3f14010b599713ca7414671944
Winter Olali <olaliwinter@gmail.com>
2026-05-11 13:55:16 +0300

    updated project

--- config/mongo.js
+++ config/mongo.js
@@ -1,21 +1,5 @@
 
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  name:  { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now },
-  password: { type: String, required: true },
-  role: {
-    type: String,
-    enum: ["admin", "member"],
-    default: "member"
-  }
-});
-
-
-const mongoose = require("mongoose");
-
 const userSchema = new mongoose.Schema({
   name:  { type: String, required: true },
   email: { type: String, required: true, unique: true },
